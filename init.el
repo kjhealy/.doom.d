381154dd069407695fa2b6fdaffa44d20b38e4e4
Kieran Healy <kjhealy@gmail.com>
2021-12-13 21:00:35 -0500

    icons and aspell

--- init.el
+++ init.el
@@ -24,7 +24,7 @@
        ;;helm              ; the *other* search engine for love and life
        ;; ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       vertico           ; the search engine of the future
+       (vertico +icons)           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
@@ -80,7 +80,7 @@
 
        :checkers
        (syntax +childframe) ; tasing you for every semicolon you forget
-       (spell +flyspell) ; tasing you for misspelling mispelling
+       (spell +flyspell +aspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
