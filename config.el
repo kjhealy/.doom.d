f005efa351ba41bdd3a8f9dc63ee99e85e34b911
Kieran Healy <kjhealy@gmail.com>
2021-12-22 17:16:43 -0500

    Polymode; windows

--- config.el
+++ config.el
@@ -356,3 +356,45 @@
 (map! "C-S-<right>" #'enlarge-window-horizontally)
 (map! "C-S-<down>" #'shrink-window)
 (map! "C-S-<up>" #'enlarge-window)
+
+(defun kjh/rotate-windows (arg)
+  "Rotate your windows; use the prefix argument to rotate the other direction"
+  (interactive "P")
+  (if (not (> (count-windows) 1))
+      (message "You can't rotate a single window!")
+    (let* ((rotate-times (prefix-numeric-value arg))
+           (direction (if (or (< rotate-times 0) (equal arg '(4)))
+                          'reverse 'identity)))
+      (dotimes (_ (abs rotate-times))
+        (dotimes (i (- (count-windows) 1))
+          (let* ((w1 (elt (funcall direction (window-list)) i))
+                 (w2 (elt (funcall direction (window-list)) (+ i 1)))
+                 (b1 (window-buffer w1))
+                 (b2 (window-buffer w2))
+                 (s1 (window-start w1))
+                 (s2 (window-start w2))
+                 (p1 (window-point w1))
+                 (p2 (window-point w2)))
+            (set-window-buffer-start-and-point w1 b2 s2 p2)
+            (set-window-buffer-start-and-point w2 b1 s1 p1)))))))
+
+(map! "C-x m" #'kjh/rotate-windows)
+
+;; Polymode
+;; Load
+(use-package! poly-R
+:config
+(map! (:localleader
+      :map polymode-mode-map
+      :desc "Export"   "e" 'polymode-export
+      :desc "Errors" "$" 'polymode-show-process-buffer
+      :desc "Weave" "w" 'polymode-weave
+      ;; (:prefix ("n" . "Navigation")
+      ;;   :desc "Next" "n" . 'polymode-next-chunk
+      ;;   :desc "Previous" "N" . 'polymode-previous-chunk)
+      ;; (:prefix ("c" . "Chunks")
+      ;;   :desc "Narrow" "n" . 'polymode-toggle-chunk-narrowing
+      ;;   :desc "Kill" "k" . 'polymode-kill-chunk
+      ;;   :desc "Mark-Extend" "m" . 'polymode-mark-or-extend-chunk)
+      ))
+  )
