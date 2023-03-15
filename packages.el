387d7b088c9095c664b26a93c2fc180fe6c241d6
Kieran Healy <kjhealy@gmail.com>
2023-03-15 19:34:20 -0400

    citeproc package for org

--- packages.el
+++ packages.el
@@ -33,6 +33,9 @@
 ;; Org-mode tables to GitHub-flavored markdown
 (package! ox-gfm)
 
+;; citeproc for Org-mode
+(package! citeproc)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
