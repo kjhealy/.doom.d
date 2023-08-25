f5f9a0a6554b007769ad4c2f4fcc3ca14fee0f5d
Kieran Healy <kjhealy@gmail.com>
2023-08-25 11:29:02 -0400

    remove tmux stuff

--- packages.el
+++ packages.el
@@ -20,8 +20,6 @@
 
 (unpin! polymode poly-R)
 
-(package! tmux-pane)
-
 ;; temporary fix for magit getting out of sync
 ;; https://emacs.stackexchange.com/questions/75827/doom-emacs-error-running-hook-global-git-commit-mode-because-void-variable
 (package! transient
